778abfe4b29651e1201ef9469e362c53e8aaa642
md_nahid_miah <nahidmiah24@gmail.com>
2025-07-24 23:54:55 +0600

    git commit -m "Initial commit"

---
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -0,0 +1,18 @@
+package com.ecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(
+		exclude = {
+				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+		}
+)
+public class ShoppingCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingCartApplication.class, args);
+	}
+
+}
