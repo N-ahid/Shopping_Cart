d5f5193eca2b0dff2555f687d0006005c79d0a24
md_nahid_miah <nahidmiah24@gmail.com>
2025-07-28 00:56:36 +0600

    Implemented add_product and category

--- src/main/java/com/ecom/ShoppingCartApplication.java
+++ src/main/java/com/ecom/ShoppingCartApplication.java
@@ -3,12 +3,13 @@ package com.ecom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-		exclude = {
-				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
-				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
-		}
-)
+@SpringBootApplication
+//		(
+//		exclude = {
+//				org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration.class,
+//				org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+//		}
+//)
 public class ShoppingCartApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/ecom/model/Product.java
@@ -0,0 +1,32 @@
+package com.ecom.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+    private String category;
+
+    private Double price;
+
+    private int stock;
+
+    private String image;
+
+}
